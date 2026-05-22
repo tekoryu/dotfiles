259f473bea6592717c80c53c5bf7d1ce84d6f5fd
tekoryu <chedou@gmail.com>
2026-05-22 16:47:19 -0300

    style: remove redundant comments from kitty, starship, fish configs

--- arch/.config/fish/config.fish
+++ arch/.config/fish/config.fish
@@ -1,9 +1,6 @@
-# Commands to run in interactive sessions can go here
 if status is-interactive
-    # No greeting
     set fish_greeting
 
-    # Use starship
     function starship_transient_prompt_func
         starship module character
     end
@@ -11,14 +8,12 @@ if status is-interactive
         starship init fish | source
         enable_transience
     end
-    
-    # Colors
+
     if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
         cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
     end
 
-    # Aliases
-    # kitty doesn't clear properly so we need to do this weird printing
+    # kitty doesn't clear scrollback properly
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias celar "printf '\033[2J\033[3J\033[1;1H'"
     alias claer "printf '\033[2J\033[3J\033[1;1H'"
